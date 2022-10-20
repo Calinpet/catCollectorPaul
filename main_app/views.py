27604bb91f653e73df08c9183e675e6b666a3a5f
Calinpet <92047573+Calinpet@users.noreply.github.com>
2022-10-20 11:36:00 -0400

    added users signup view function/form

--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,8 @@ from pyexpat import model
 from django.shortcuts import render, redirect
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Cat, Toy
 from .forms import FeedingForm
@@ -46,10 +48,37 @@ def assoc_toy(request, cat_id, toy_id):
    Cat.objects.get(id=cat_id).toys.add(toy_id)
    return redirect('detail', cat_id=cat_id)
 
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    # This is how to create a 'user' form object
+    # that includes the data from the browser
+     form = UserCreationForm(request.POST)
+     if form.is_valid():
+      # This will add the user to the database
+      user = form.save()
+      # This is how we log a user in via code
+      login(request, user)
+      return redirect('index')
+     else:
+      error_message = 'Invalid sign up - try again'
+  # A bad POST or a GET request, so render signup.html with an empty form
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)   
+
 class CatCreate(CreateView):
     model = Cat
     fields = ['name', 'breed', 'description', 'age']
     # success_url = '/cats/' 
+
+    # This inherited method is called when a
+    # valid cat form is being submitted
+    def form_valid(self, form):
+    # Assign the logged in user (self.request.user)
+        form.instance.user = self.request.user  # form.instance is the cat
+    # Let the CreateView do its job as usual
+        return super().form_valid(form)
 
 class CatUpdate(UpdateView):
     model = Cat
