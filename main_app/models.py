759a5eae79462b555e45b54533ee10539287e08d
Calinpet <92047573+Calinpet@users.noreply.github.com>
2022-10-20 11:10:57 -0400

    added users auth, updated navbar dynamically

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.urls import reverse
+from datetime import date
+# Import the User
+from django.contrib.auth.models import User
 
 MEALS = (
     ('B', 'Breakfast'),
@@ -20,7 +23,8 @@ class Cat(models.Model):
     description = models.TextField(max_length=250)
     age = models.IntegerField()
     toys = models.ManyToManyField(Toy)
-    
+    # Add the foreign key linking to a user instance
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     # changes to instance methods do not require re-generation / running of migrations
     def __str__(self):
         return self.name
